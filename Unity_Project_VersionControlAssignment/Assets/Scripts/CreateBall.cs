97fc22e6edbb02a3092b2f5cccb8f25e91693967
Charlie Harper <charlottehdesigns@gmail.com>
2019-11-05 18:41:07 -0500

    Spawning new balls is now possible, also you can kill the ball in the magenta cube -spawn script -kill script -kill object

---
+++ Unity_Project_VersionControlAssignment/Assets/Scripts/CreateBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBall : MonoBehaviour
+{
+    public GameObject theBall;
+    public Transform spawnPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Instantiate(theBall, spawnPosition);
+
+        }
+    }
+}
