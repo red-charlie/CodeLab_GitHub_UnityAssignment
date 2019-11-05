f4c5f83a7bd5e062b4fbc097c4f33332e6d81496
Charlie Harper <charlottehdesigns@gmail.com>
2019-11-05 18:27:59 -0500

    Added in Ball, ground, and controller scripts -Ball game object added -Ground game object added -Materials created for both ball and ground -created WASD controller script

---
+++ Unity_Project_VersionControlAssignment/Assets/Scripts/WASDController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASDController : MonoBehaviour
+{
+    public float force = 20f;
+    public KeyCode UpButton;
+    public KeyCode DownButton;
+    public KeyCode LeftButton;
+    public KeyCode RightButton;
+    Rigidbody rb;
+
+    //establishing the variables
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); //this is the rigidbody of the object
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool hasInput = false;
+        
+        if(Input.GetKeyDown(UpButton)){
+            hasInput = true;
+            rb.velocity = new Vector3(0, 0, force); //moving this into z axis
+
+        }
+
+        if(Input.GetKeyDown(DownButton)){
+            hasInput = true;
+            rb.velocity = new Vector3 (0, 0, -force); //moving the other way
+
+        }
+
+        if(Input.GetKeyDown(LeftButton)){
+            hasInput = true;
+            rb.velocity = new Vector3 (-force, 0, 0); //moving it left
+
+        }
+
+        if(Input.GetKeyDown(RightButton)){
+            hasInput = true;
+            rb.velocity = new Vector3 (force, 0, 0); //moving it right
+
+        }
+//if not pressing anykey
+        if(!Input.anyKey){
+            rb.velocity = rb.velocity * 0.9f; // decrease the velocity
+
+        }
+    }
+}
